53407c923b883485c40d8d8ed76b1cd3036aae0f
zfac131 <zfac131@live.rhul.ac.uk>
2024-11-04 11:19:44 +0900

    add code for excel

--- gps_vss_to_graph.py
+++ gps_vss_to_graph.py
@@ -2,9 +2,10 @@ import json
 import matplotlib.pyplot as plt
 import os
 import cv2
+import pandas as pd
 
-json_folder = 'jsonexample'
-ex_json_list = os.listdir(json_folder)
+json_root = 'jsonexample'
+ex_json_list = os.listdir(json_root)
 
 json_root = 'G:/'
 can_loc_list = os.listdir(json_root)
@@ -100,12 +101,53 @@ def check_sudden_action(data_vss_100ms):
 
     return [sudden_acc, sudden_dec, sudden_stop, sudden_start]
 
+def print_has_all(json_data_list):
+    has_alls = []
+
+    for can_json in json_data_list:
+        if can_json[7]:
+            has_alls.append(can_json[0])
+
+    for has_all in has_alls:
+        print(has_all)
+        
+def filename_only(filename):
+    return filename.split('/')[-1]
+
+def print_has_any(json_data_list):
+    for can_json in json_data_list:
+        print(f'{filename_only(can_json[0])}: {can_json[6]}')
+
+def has_any_to_excel(json_data_list):
+    filenames = []
+    sudden_acc = []
+    sudden_dec = []
+    sudden_stops = []
+    sudden_starts = []
+
+    for can_json in json_data_list:
+        filenames.append(filename_only(can_json[0]))
+        sudden_acc.append(can_json[6][0])
+        sudden_dec.append(can_json[6][1])
+        sudden_stops.append(can_json[6][2])
+        sudden_starts.append(can_json[6][3])
+
+    sudden_actions_data = pd.DataFrame()
+    sudden_actions_data['파일이름'] = filenames
+    sudden_actions_data['급가속'] = sudden_acc
+    sudden_actions_data['급감속'] = sudden_dec
+    sudden_actions_data['급정지'] = sudden_stops
+    sudden_actions_data['급출발'] = sudden_starts
+
+    sudden_actions_data.to_excel('can_to_excel/241104_위험행동.xlsx', index=False)
+
+
 n_of_can = 0
 e1 = cv2.getTickCount()
-for i in source_can_list:
+for json_name in source_can_list:
     n_of_can += 1
-    # with open(f'{json_folder}/{i}', 'r') as f:
-    with open(i, 'r') as f:
+    # with open(f'{json_root}/{json_name}', 'r') as f:
+    with open(json_name, 'r') as f:
         data = json.load(f)
 
         frames = data['frames']
@@ -117,30 +159,23 @@ for i in source_can_list:
         gps_vss_seconds = frames_to_seconds(gps_vss_frame)
 
         gps_vss_100ms = frames_to_100ms(gps_vss_frame)
-
-#         # for test
-#         gps_vss_100ms = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 
-# 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 21, 21, 21, 21, 21, 21, 21, 21, 21, 21, 21, 21, 21, 21, 21, 21, 21, 21, 21, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 23, 24, 24, 24, 24, 24, 24, 24, 24, 24, 27, 27, 27, 27, 27, 27, 27, 27, 27, 27, 29, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 34, 
-# 34, 34, 34, 34, 34, 34, 34, 34, 34, 36, 36, 36, 36, 36, 36, 36, 36, 36, 36, 37, 39, 39, 39, 39, 39, 39, 39, 39, 39, 42, 42, 42, 42, 42, 42, 42, 42, 42, 42, 42, 43, 43, 43, 43, 43, 43, 43, 43, 45, 45, 45, 45, 45, 45, 45, 45, 45, 45, 44, 44, 44, 44, 44, 44, 44, 44, 44, 44, 44, 45, 45, 45, 45, 45, 45, 45, 45, 45, 45, 46, 46, 46, 46, 46, 46, 46, 46, 46, 46, 46, 46, 46, 46, 46, 46, 46, 46, 46, 46, 45, 45, 45, 45, 45, 45, 45, 45, 45, 45, 42, 42, 42, 42, 42, 42, 42, 42, 39, 38, 38, 38, 38, 38, 38, 38, 38, 38, 38, 34, 34, 34, 34, 34, 34, 34, 34, 34, 34, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 31, 31, 31, 
-# 31, 31, 31, 31, 31, 31, 31, 31, 28, 28, 28, 28, 28, 28, 28, 28, 28, 25, 21, 21, 21, 21, 21, 21, 21, 21, 21, 15, 15, 15, 15, 15, 15, 15, 15, 15, 10, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 
-# 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 
-# 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 20, 20, 20, 20, 20, 20, 20, 20, 20, 20, 21, 25, 25, 25, 25, 25, 25, 25, 26, 29, 29, 29, 29, 29, 29, 29, 29, 29, 29, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 36, 38, 38, 
-# 38, 38, 38, 38, 38, 38, 38, 38, 40, 40, 40, 40, 40, 40, 40, 40, 40, 40, 40, 40, 40, 40, 40, 40, 40, 40, 40, 39, 39, 39, 39, 39, 39, 39, 39, 39, 39, 39, 39, 39, 39, 39, 39, 39, 39, 39, 40, 41, 41, 41, 41, 41, 41, 41, 41, 41, 41, 42, 42, 42, 42, 42, 42, 42, 42, 42, 42, 43, 43, 43, 43, 43, 43, 43, 43, 43, 43, 43, 43, 43, 43, 43, 43, 43, 43, 43, 43, 41, 40, 40, 40, 40, 40, 40, 40, 40, 40, 39, 39, 39, 39, 39, 39, 39, 39, 39, 39, 39, 40, 40, 40, 40, 40, 40, 40, 40, 40, 39, 39, 39, 39, 39, 39, 39, 39, 39, 39, 39, 41, 41, 41, 41, 41, 41, 41, 41, 41, 41, 41, 41, 41, 41, 41, 41, 41, 41, 41, 42, 42, 42, 42, 42, 42, 42, 42, 42, 42, 44, 44, 44, 44, 
-# 44, 44, 44, 44, 44, 43, 43, 43, 43, 43, 43, 43, 43, 43, 43, 43, 44, 44, 44, 44, 44, 44, 44, 44, 44, 44, 44, 44, 44, 44, 44, 44, 44, 44, 44, 44, 43, 41, 41, 41, 41, 41, 41, 41, 41, 41, 41, 43, 43, 43, 43, 43, 43, 43, 43, 43, 43, 42, 42, 42, 42, 42, 42, 42, 42, 42, 42, 41, 41, 41, 41, 41, 41, 41, 41, 41, 40, 40, 40, 40, 40, 40, 40, 40, 40, 40, 38, 38, 38, 38, 38, 38, 38, 38, 38, 38, 38, 38, 38, 38, 38, 38, 38, 38, 38, 38, 29, 29, 29, 29, 29, 29, 29, 29, 21, 21, 21, 21, 21, 21, 21, 21, 21, 21, 21, 21, 21, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6]
-        
-        # gps_vss_seconds = ms_to_seconds(gps_vss_100ms)
         
         gps_vss_acceleration = vss_to_acceleration(gps_vss_seconds)
         # gps_vss_acceleration = vss_to_acceleration(gps_vss_frame)
         # gps_vss_acceleration = vss_to_acceleration(gps_vss_100ms)
         
-        have_all = False
+        has_all = False
         sudden_actions = check_sudden_action(gps_vss_100ms)
 
-        if sudden_actions[0] and sudden_actions[1] and sudden_actions[2] and sudden_actions[3]:
-            have_all = True
+        bool_sudden_actions = ['X', 'X', 'X', 'X']
 
-        json_data_list.append([i, gps_vss_seconds, gps_vss_acceleration, gps_vss_frame, gps_vss_100ms, sudden_actions, have_all])
+        for i in range(len(sudden_actions)):
+            bool_sudden_actions[i] = 'O' if sudden_actions[i] else 'X'
+
+        if sudden_actions[0] and sudden_actions[1] and sudden_actions[2] and sudden_actions[3]:
+            has_all = True
+
+        json_data_list.append([json_name, gps_vss_seconds, gps_vss_acceleration, gps_vss_frame, gps_vss_100ms, sudden_actions, bool_sudden_actions, has_all])
 
     print(f'scanning... {n_of_can}/{len(source_can_list)}')
 
@@ -150,14 +185,11 @@ Total_time = (e2 - e1)/ cv2.getTickFrequency()
 print(f'Total time taken: {Total_time} seconds')
 print(f'Total scanned number of can_data: {n_of_can} files')
 
-have_alls = []
 
-for can_json in json_data_list:
-    if can_json[6]:
-        have_alls.append(can_json[0])
+print_has_all(json_data_list)
+print_has_any(json_data_list)
 
-for have_all in have_alls:
-    print(have_all)
+has_any_to_excel(json_data_list)
 
 json_filename = json_data_list[0][0]
 data_vss = json_data_list[0][1]
@@ -165,7 +197,8 @@ data_acc = json_data_list[0][2]
 data_frame = json_data_list[0][3]
 data_vss_100ms = json_data_list[0][4]
 sudden_actions = json_data_list[0][5]
-have_all = json_data_list[0][6]
+bool_sudden_actions = json_data_list[0][6]
+has_all = json_data_list[0][7]
 
 x1 = [i for i in range(len(data_vss))]
 # x1 = [i for i in range(len(data_frame))]
@@ -193,7 +226,6 @@ for i in sudden_stops:
 for i in sudden_starts:
     print(f'sudden start: {i}')
 
-# print(json_filename, have_all)
 # print(data_frame)
 '''
 graph
