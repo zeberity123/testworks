f20cfe46e53f9ae0876480ee391ba517f450bad9
zfac131 <zfac131@live.rhul.ac.uk>
2024-11-21 15:55:06 +0900

    fix for 6kb error

--- hwp_converter/hwp_converter.py
+++ hwp_converter/hwp_converter.py
@@ -3,10 +3,10 @@ import win32com.client as win32
 import win32gui
 import cv2
 
-hwp_root = 'C:/Users/Darudayu/Desktop/hwp_1/asdfa'
+hwp_root = 'C:/Users/Darudayu/Desktop/hwp_1/hwp_1'
 hwp_list = os.listdir(hwp_root)
-pdf_dest = 'C:/Users/Darudayu/Desktop/hwp_1/pdfs'
-hwp = win32.Dispatch('HWPFrame.HwpObject')
+pdf_dest = 'C:/Users/Darudayu/Desktop/hwp_1/pdfs_1'
+hwp = win32.gencache.EnsureDispatch('HWPFrame.HwpObject')
 print(hwp)
 hwnd = win32gui.FindWindow(None, '빈 문서 1 - 한글')
 print(hwnd)
@@ -14,20 +14,18 @@ win32gui.ShowWindow(hwnd, 0)
 hwp.RegisterModule('FilePathCheckDLL', 'FilePathCheckerModule')
 
 cnt = 0
-for hwp_file in hwp_list[:10]:
+for hwp_file in hwp_list[1704:]:
     e1 = cv2.getTickCount()
     cnt += 1
     print(f'converting... {cnt}/{len(hwp_list)}')
-
-    hwp.Open(f'{hwp_root}/{hwp_file}', Format='HWP', arg='')
-    hwp.HAction.GetDefault('FileSaveAsPdf', hwp.HParameterSet.HFileOpenSave.HSet)
-    hwp.HParameterSet.HFileOpenSave.filename = os.path.join(pdf_dest, hwp_file.replace('.hwp', '.pdf'))
-    hwp.HParameterSet.HFileOpenSave.Format = 'PDF'
-    hwp.HAction.Execute('FileSaveAsPdf', hwp.HParameterSet.HFileOpenSave.HSet)
-
+    print(hwp_file)
+    hwp.Open(f'{hwp_root}/{hwp_file}')
+    hwp.HParameterSet.HFileOpenSave.filename = f'{pdf_dest}/{hwp_file.split(".")[0]}.pdf'
+    hwp.HParameterSet.HFileOpenSave.Format = "PDF"
+    hwp.HAction.Execute("FileSaveAs_S", hwp.HParameterSet.HFileOpenSave.HSet)
 
     e2 = cv2.getTickCount()
-print(f'Time taken: {(e2-e1)/cv2.getTickFrequency()} seconds')
+    print(f'Time taken: {(e2-e1)/cv2.getTickFrequency()} seconds')
 
 hwp.Quit()
 del hwp
